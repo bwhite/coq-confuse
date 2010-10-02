417f4147302130920942787f10dd310bb2a7f7ab
brandyn <brandyn@router.(none)>
2010-10-02 19:57:52 -0400

    Updated, works!

--- main.v
+++ main.v
@@ -18,15 +18,30 @@ Notation "[-]" := niln.
 
 Fixpoint numposp (x: posg) : nat :=
   match x with
-    | nilp => 1
+    | nilp => 0
     | (mp h) +:: t => (numposn t) + h + 1
   end
   with numposn (x: negg) : nat :=
   match x with
     | niln => 0
-    | (mn h) -:: t => (numposp t)
+    | _ -:: t => (numposp t)
   end.
 
+Fixpoint numnegp (x: posg) : nat :=
+  match x with
+    | nilp => 0
+    | _ +:: t => (numnegn t)
+  end
+  with numnegn (x: negg) : nat :=
+  match x with
+    | niln => 0
+    | (mn h) -:: t => (numnegp t) + h + 1
+  end.
+
+
 Eval simpl in numposp ((mp 6) +:: (mn 5) -:: [+]).
+Eval simpl in numnegn ((mn 6) -:: (mp 5) +:: [-]).
+Eval simpl in numnegp ((mp 6) +:: (mn 5) -:: [+]).
+Eval simpl in numposn ((mn 6) -:: (mp 5) +:: [-]).
 Eval simpl in  (mp 6) +:: (mn 5) -:: [+].
 Eval simpl in (mn 0) -:: [+].
