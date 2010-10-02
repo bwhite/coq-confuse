4b1d5bfa38fdf6b96d82e9290a0c44fd9386aec8
brandyn <brandyn@router.(none)>
2010-10-02 18:53:18 -0400

    First

---
+++ main.v
@@ -0,0 +1,20 @@
+Eval simpl in S 1.
+
+Inductive poss : Type :=
+  | mp: nat -> poss.
+
+Inductive negs : Type :=
+  | mn : nat -> negs.
+
+Inductive posg :=
+| nilp : posg
+| consp : poss -> negg -> posg
+with negg:=
+| niln : negg
+| consn : negs -> posg -> negg.
+
+Notation "x +:: l" := (consp x l) (at level 60, right associativity).
+Notation "x -:: l" := (consn x l) (at level 60, right associativity).
+Notation "[+]" := nilp.
+Notation "[-]" := niln.
+Eval simpl in  (mp 6)+:: (mn 5) -:: nilp.
