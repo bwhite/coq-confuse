20d78b89db6bcc4e859903bc83e0d21d84658dcc
brandyn <brandyn@router.(none)>
2010-10-02 19:40:10 -0400

    Added an example of computing the number of positive in a group

--- main.v
+++ main.v
@@ -17,4 +17,20 @@ Notation "x +:: l" := (consp x l) (at level 60, right associativity).
 Notation "x -:: l" := (consn x l) (at level 60, right associativity).
 Notation "[+]" := nilp.
 Notation "[-]" := niln.
+
+Fixpoint numposp (x: posg) : nat :=
+  match x with
+    | nilp => 1
+    | (mp h) +:: t => (numposn t) + h + 1
+  end
+  with numposn (x: negg) : nat :=
+  match x with
+    | niln => 0
+    | (mn h) -:: t => (numposp t) + 1
+  end.
+
+Eval simpl in numposp ((mp 6) +:: (mn 5) -:: [+]).
+
 Eval simpl in  (mp 6) +:: (mn 5) -:: [+].
+Eval simpl in (mn 0) -:: [+].
+Eval simpl in {x : nat | x > 0}.
