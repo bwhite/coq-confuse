1fda6645b0d26de626d5038ea5c624216ca2928f
brandyn <brandyn@router.(none)>
2010-10-02 20:08:58 -0400

    Added obvious proofs about adding groups and their effect on the final count

--- main.v
+++ main.v
@@ -21,7 +21,7 @@ Fixpoint numposp (x: posg) : nat :=
     | nilp => 0
     | (mp h) +:: t => (numposn t) + h + 1
   end
-  with numposn (x: negg) : nat :=
+with numposn (x: negg) : nat :=
   match x with
     | niln => 0
     | _ -:: t => (numposp t)
@@ -32,12 +32,11 @@ Fixpoint numnegp (x: posg) : nat :=
     | nilp => 0
     | _ +:: t => (numnegn t)
   end
-  with numnegn (x: negg) : nat :=
+with numnegn (x: negg) : nat :=
   match x with
     | niln => 0
     | (mn h) -:: t => (numnegp t) + h + 1
   end.
-
 
 Eval simpl in numposp ((mp 6) +:: (mn 5) -:: [+]).
 Eval simpl in numnegn ((mn 6) -:: (mp 5) +:: [-]).
@@ -45,3 +44,15 @@ Eval simpl in numnegp ((mp 6) +:: (mn 5) -:: [+]).
 Eval simpl in numposn ((mn 6) -:: (mp 5) +:: [-]).
 Eval simpl in  (mp 6) +:: (mn 5) -:: [+].
 Eval simpl in (mn 0) -:: [+].
+
+Theorem add_n_to_p_count_p : forall (n:nat) (l:posg), numposn((mn n) -:: l) = numposp l.
+Proof. intros. reflexivity. Qed.
+
+Theorem add_n_to_p_count_n : forall (n:nat) (l:posg), numnegn((mn n) -:: l) = (numnegp l) + n + 1.
+Proof. intros. reflexivity. Qed.
+
+Theorem add_p_to_n_count_p : forall (n:nat) (l:negg), numposp((mp n) +:: l) = (numposn l) + n + 1.
+Proof. intros. reflexivity. Qed.
+
+Theorem add_p_to_n_count_n : forall (n:nat) (l:negg), numnegp((mp n) +:: l) = (numnegn l).
+Proof. intros. reflexivity. Qed.
